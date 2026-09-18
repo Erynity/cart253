78c16b92efec581ce66ee54e516d10ed5832b2e0
Eryne <eryneycn@gmail.com>
2026-09-17 22:34:13 -0400

    prototype 2 | created square canvas, set a background color, then added 3 circles with thick stroke weight and changed each color

--- topics/prototype/instructions/instructions-prototype2/js/script.js
+++ topics/prototype/instructions/instructions-prototype2/js/script.js
@@ -11,7 +11,7 @@
  * Creates canva
 */
 function setup() {
-
+    createCanvas(600, 600);
 }
 
 
@@ -19,5 +19,25 @@ function setup() {
  * Draws the landscape
 */
 function draw() {
-
+    background('#51d866');
+    // 1st circle
+    push();
+    fill('#f1bbd0');
+    stroke('#3d4e72');
+    strokeWeight(150);
+    ellipse(40, 40,800, 800);
+    pop();
+    // 2nd circle
+    push();
+    fill('#da5757');
+    stroke('#ffd000');
+    strokeWeight(100);
+    ellipse(200, 600, 300, 300);
+    pop();
+    // 3rd circle
+    push();
+    fill('#da57b1');
+    stroke('#ff0000');
+    strokeWeight(100);
+    ellipse(500, 200, 300, 300);
 }
