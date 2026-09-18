6503c207b6f964729473d1876f5dbd3f0dedb24e
Eryne <eryneycn@gmail.com>
2026-09-17 22:42:47 -0400

    prototype 3 | broken TV effect with rainbow colors and a black canvas border !

--- topics/prototype/instructions/instructions-prototype3/js/script.js
+++ topics/prototype/instructions/instructions-prototype3/js/script.js
@@ -11,7 +11,7 @@
  * Creates canva
 */
 function setup() {
-
+    createCanvas(700, 600);
 }
 
 
@@ -19,5 +19,55 @@ function setup() {
  * Draws the landscape
 */
 function draw() {
+    background('#737373');
 
+
+    push();
+    fill('#ff0000');
+    noStroke();
+    rect(0, 0, 100, 600);
+    pop();
+
+    push();
+    fill('#ff8400');
+    noStroke();
+    rect(100, 0, 100, 600);
+    pop();
+
+    push();
+    fill('#f2ff00');
+    noStroke();
+    rect(200, 0, 100, 600);
+    pop();
+
+    push();
+    fill('#00ff0d');
+    noStroke();
+    rect(300, 0, 100, 600);
+    pop();
+
+    push();
+    fill('#0077ff');
+    noStroke();
+    rect(400, 0, 100, 600);
+    pop();
+
+    push();
+    fill('#7b00ff');
+    noStroke();
+    rect(500, 0, 100, 600);
+    pop();
+
+    push();
+    fill('#ff00a2');
+    noStroke();
+    rect(600, 0, 100, 600);
+    pop();
+    
+    push();
+    fill('#ffffff00');
+    stroke('#0d0d0d');
+    strokeWeight(20);
+    rect(0, 0, 700, 600);
+    pop();
 }
